31ba7c67cd5b001490b161316f48960aba8a47e6
张新永 <xinyong.zhang@zen-x.com.cn>
2022-01-11 11:39:42 +0800

    feat: 创建项目

---
+++ 2022-01-11-ws/myapp-service/dist/main.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@nestjs/core");
+const app_module_1 = require("./app.module");
+const port = '3000';
+async function bootstrap() {
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    await app.listen(port);
+    console.log(`${port}端口启动`);
+}
+bootstrap();
+//# sourceMappingURL=main.js.map
---
+++ 2022-01-11-ws/myapp-service/src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+const port = '3000';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(port);
+  console.log(`${port}端口启动`);
+
+}
+bootstrap();
